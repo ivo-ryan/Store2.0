329d73b55340f31100cf7b56becae8db637b4f99
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-10-04 22:44:56 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import Header from "@/components/header";
 import styles from "./page.module.scss";
 import CategoriesSection from "@/components/categories/categorySection";
+import FeaturedProductsSection from "@/components/FeaturedProduct/featuredProductSection";
 
 export default function Home() {
   return (
     <div className={styles.page}>
         <Header/>
         <CategoriesSection/>
+        <FeaturedProductsSection/>
     </div>
   );
 }
---
+++ src/components/FeaturedProduct/productCard/index.tsx
@@ -0,0 +1,51 @@
+
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import { FiShoppingCart, FiHeart } from "react-icons/fi";
+
+interface ProductProps {
+  id: number;
+  name: string;
+  price: number;
+  oldPrice?: number;
+  image: string;
+  rating: number;
+  isNew?: boolean;
+}
+
+export default function ProductCard({
+  name,
+  price,
+  oldPrice,
+  image,
+  rating,
+  isNew,
+}: ProductProps) {
+  return (
+    <div className={styles.card}>
+      {isNew && <span className={styles.newTag}>NOVO</span>}
+      <button className={styles.favorite}>
+        <FiHeart />
+      </button>
+
+      <img src={image} alt={name} width={300} height={300} />
+
+      <h3>{name}</h3>
+
+      <div className={styles.rating}>
+        {"★".repeat(rating)}
+        {"☆".repeat(5 - rating)}
+      </div>
+
+      <div className={styles.price}>
+        <span className={styles.current}>${price.toFixed(2)}</span>
+        {oldPrice && <span className={styles.old}>${oldPrice.toFixed(2)}</span>}
+      </div>
+
+      <button className={styles.cartButton}>
+        <FiShoppingCart />
+        Adicionar ao Carrinho
+      </button>
+    </div>
+  );
+}
